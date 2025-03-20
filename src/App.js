98980b8a1576dd0f686f28e0258f47f9a0ce87b3
oabnerwilliam <abnerwilliam026@gmail.com>
2025-03-20 08:32:03 -0300

    Commit de teste

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
+
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Container customClass="min-height">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="company" element={<Company/>} />
+            <Route path="contact" element={<Contact/>} />
+            <Route path="newproject" element={<NewProject/>} />
+            <Route path="projects" element={<Projects/>}/>
+          </Routes>
+        </Container>  
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App
